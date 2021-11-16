fb602211cd3159abe717a60fb5eba640a4abba45
DeniseIra02 <irajubilo@gmail.com>
2021-11-16 20:57:47 +0800

    Program1 successfully start a functional condition.

--- Prog1.py
+++ Prog1.py
@@ -1,2 +1,7 @@
 import math
 grade_ = float(input("Enter yout grade: "))
+yourGrd_ = math.ceil (grade_)
+
+if yourGrd_ >= 97 and yourGrd_ <= 100:
+    print("Grade/Mark: 1.00")
+    print("Description: Excellent")
