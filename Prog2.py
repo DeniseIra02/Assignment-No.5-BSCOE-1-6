8e0dd51eb768bb420219198ca41c9440d0f4d31e
DeniseIra02 <irajubilo@gmail.com>
2021-11-17 15:24:11 +0800

    Program2 have a complete conditions within the define function.

--- Prog2.py
+++ Prog2.py
@@ -2,3 +2,12 @@ def getThreeNum():
     firstN = int(input("First Number: "))
     secondN = int(input("Second Number: "))
     thirdN = int(input("Third Number: "))
+    if firstN < secondN and firstN < thirdN:
+        print(f"{firstN}is the lowest number.")
+        return firstN
+    elif secondN < firstN and secondN < thirdN:
+        print(f"{secondN}is the lowest number.")
+        return secondN
+    else:
+        print(f"{thirdN}is the lowest number.")
+        return thirdN
