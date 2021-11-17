ca3d61106eb50b4f9c792540e2659731d84844a1
DeniseIra02 <irajubilo@gmail.com>
2021-11-17 15:34:32 +0800

    Program2 complete. Successfully working with display of the lowest number after calling the define function using a global variable.

--- Prog2.py
+++ Prog2.py
@@ -3,11 +3,15 @@ def getThreeNum():
     secondN = int(input("Second Number: "))
     thirdN = int(input("Third Number: "))
     if firstN < secondN and firstN < thirdN:
-        print(f"{firstN}is the lowest number.")
+        print(f"{firstN} is the lowest number.")
         return firstN
     elif secondN < firstN and secondN < thirdN:
-        print(f"{secondN}is the lowest number.")
+        print(f"{secondN} is the lowest number.")
         return secondN
     else:
-        print(f"{thirdN}is the lowest number.")
+        print(f"{thirdN} is the lowest number.")
         return thirdN
+
+three_Num = getThreeNum()
+
+print("Done")
