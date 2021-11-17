4466b6044a8d84588fb0e06aa8b750323ddd01a2
DeniseIra02 <irajubilo@gmail.com>
2021-11-17 15:55:04 +0800

    Program3 complete. It is working successfully with the display of the life stage of the person. Conditions are complete.

--- Prog3.py
+++ Prog3.py
@@ -1,2 +1,14 @@
 your_Age = int(input("Age: "))
 
+if your_Age >= 0 and your_Age <= 12:
+    print ("Kid")
+elif your_Age >= 13 and your_Age <= 17:
+    print("Teen")
+elif your_Age == 18:
+    print("Debut")
+elif your_Age >= 19 and your_Age <= 59:
+    print("Adult")
+else:
+    print("Senior")
+
+print("Done")
